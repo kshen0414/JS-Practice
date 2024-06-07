e9a25365d696233435f346f5a7b5988496b7dfc7
kok shen <kokshen3414@gmail.com>
2024-06-07 21:14:52 +0800

    finished the solution of a live solution

--- interview_online/1.int_arr.js
+++ interview_online/1.int_arr.js
@@ -1,6 +1,6 @@
 // given an integer array, find the largest and smallest index
 // if the smallest value's index is larger than the max value in the array, return 0
-// else return largest and smallest index
+// else return largest element's and smallest element's index 
 // note this is a real interview question from ANT group
 
 // the idea is that we would need to find the max and min value right?
@@ -13,10 +13,17 @@ function int_arr(arr){
     // now after getting the direct min and max value
     // how do i retrieve the index
 
-    console.log(min);
-
-    return 1;
-
+    if(arr.indexOf(min) > max){
+        return 0;
+    } 
+    
+    else{
+        // reference, in javascript we can return an array of values
+        // 2nd method is to return an object containing our values
+        // https://stackoverflow.com/questions/2917175/return-multiple-values-in-javascript
+        // https://www.geeksforgeeks.org/javascript-return-multiple-values-from-function/
+        return [arr.indexOf(min),arr.indexOf(max)];
+    }
 
 }
 
