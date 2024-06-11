e9f90938cb3eb2a5375bbc6f6d6aa44fc1d5309a
kok shen <kokshen3414@gmail.com>
2024-06-11 22:13:56 +0800

    trying the logic of a number guessing game

--- interview_practice/8.is_nested_array.js
+++ interview_practice/8.is_nested_array.js
@@ -1,4 +1,4 @@
 // Write a JavaScript program to find the largest element in a nested array. 
-
+// i guess an assumption, we would give it is that we are iterating through a set of numbers?
 
 // will check again later
