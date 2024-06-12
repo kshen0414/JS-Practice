6d94b87a0104bf91939c2f8ebffe50cce778f6a9
kok shen <kokshen3414@gmail.com>
2024-06-12 21:29:29 +0800

    added the solution for is_nested array

--- interview_practice/8.is_nested_array.js
+++ interview_practice/8.is_nested_array.js
@@ -2,3 +2,43 @@
 // i guess an assumption, we would give it is that we are iterating through a set of numbers?
 
 // will check again later
+// *hint, to access nested elements...we use more indexes arr[i][j][k]
+// so the next approach is to obtain the depth of the array
+// research from stack overflow suggests that we use a method called flat?
+// https://stackoverflow.com/questions/55420156/get-arrays-depth-in-javascript
+
+// more documentation about using array.flat()
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+
+var arr = [1,2,3,[2,999,0],[899,[3,4,1000]],8];
+
+function nest_arr(arr){
+    var flat_arr = arr.flat(Infinity);
+    return (Math.max(...flat_arr));
+}
+
+console.log(nest_arr(arr));
+
+
+// suggested solution makes an assumption of a depth of 2
+// function findLargestElement(nestedArray) { 
+
+//     let largest = nestedArray[0][0]; 
+
+//     for (let arr of nestedArray) { 
+
+//       for (let num of arr) { 
+
+//         if (num > largest) { 
+
+//           largest = num; 
+  
+//         } 
+  
+//       } 
+  
+//     } 
+  
+//     return largest; 
+  
+//   } 
---
+++ interview_practice/9.fibonacci.js
@@ -0,0 +1,1 @@
+// Write a JavaScript function that returns the Fibonacci sequence up to a given number of terms. 
